2e21839fcbcb801917d8cc25952d590595ac035a
tanhakabir <tanhakabir.ca@gmail.com>
2021-06-24 11:36:51 -0400

    Add example notebook using serializer

---
+++ notebook-serializer/src/extension/extension.ts
@@ -0,0 +1,21 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+import { SampleContentSerializer, SampleKernel } from './sampleProvider';
+
+// This method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  
+  context.subscriptions.push(
+    vscode.workspace.registerNotebookSerializer(
+      'test-notebook-renderer', new SampleContentSerializer(), { transientOutputs: true }
+    ),
+    new SampleKernel()
+  );
+  
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
